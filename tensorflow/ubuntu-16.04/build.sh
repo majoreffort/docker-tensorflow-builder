e951fdc0dfb6439c06afa9ddcb35f6e44ae40a6c
majoreffort <41589814+majoreffort@users.noreply.github.com>
2018-11-12 18:37:20 +0100

    Update build.sh

--- tensorflow/ubuntu-16.04/build.sh
+++ tensorflow/ubuntu-16.04/build.sh
@@ -13,7 +13,7 @@ gcc --version
 conda create --yes -n tensorflow python==$PYTHON_VERSION
 source activate tensorflow
 conda install --yes six numpy wheel mock bazel=0.18.0
-conda install --yes --no-deps -c conda-forge keras-applications==1.0.5 keras_preprocessing==1.0.3
+conda install --yes --no-deps -c conda-forge keras-applications keras_preprocessing
 
 # Compile TensorFlow
 
